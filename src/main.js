9f67dab8291cdcaa4ec265fb0c30b83bc1b932a0
Kody Gordon <gordon.km89@gmail.com>
2021-06-30 23:34:01 -0700

    Finished logic for main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import $ from 'jquery'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/styles.css'
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Person from './js/galactic-age.js';
 
-$("section").text("TEST TEXT INPUT")
+$(document).ready(() => {
+  let user;
+  $("form#userInput").submit((event) => {
+    event.preventDefault();
+    const age = parseInt($("input#ageInput").val());
+    const planet = $("select#planetSelect").val();
+    const gender = $("input:radio[name=genderRadio]:checked").val();
+    const activityLevel = $("input:radio[name=activityRadio]:checked").val();
+
+    user = new Person(age, gender, activityLevel);
+    $("#galaxyAge").text(user.getGalaxyAge(planet));
+    $("#lifeExpectancyResult").text(user.getGalaxyLifeExpectancy(planet));
+    $("#results").show();
+    $("#seeAllLink").show();
+  });
+
+  $("#seeAllLink").click(() => {
+    $("tr#appendedTableRows").remove();
+    $("#statsTable").show();
+    for (const planet in user.galaxyAges) {
+      $("#statsTable").append(`<tr id="appendedTableRows"><td>${planet}</td><td>${Math.round(user.galaxyAges[planet])}</td><td>${Math.round(user.galaxyLifeExpectancies[planet])}</td><td>${Math.round(user.galaxyLifeExpectancies[planet] - user.galaxyAges[planet])}</td></tr>`);
+    }
+  });
+});
